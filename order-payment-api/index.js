71d81385ebc7ebde191099583604c8da1955828c
NandikaKW <nandikakaweesha4005@gmail.com>
2026-08-09 11:56:36 +0530

    Add Eureka service discovery to order payment API

--- order-payment-api/index.js
+++ order-payment-api/index.js
@@ -2,14 +2,44 @@ const express = require('express');
 const mongoose = require('mongoose');
 const OrderRoutes = require('./routes/OrderRoutes');
 const bodyParser = require("body-parser");
+const {Eureka} = require("eureka-js-client");
 
 const app = express();
 const PORT = 3000;
+const EUREKA_SERVER = 'localhost';
 
 // Middleware
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Eureka Client
+const eurekaClient = new Eureka({
+    instance: {
+        app: 'order-payment-api',
+        instanceId: `order-payment-api:${PORT}`,
+        hostName: 'localhost',
+        ipAddr: '127.0.0.1',
+        port: {
+            '$': PORT,
+            '@enabled': true
+        },
+        vipAddress: 'order-payment-api',
+        statusPageUrl: `http://localhost:${PORT}/status`,
+        dataCenterInfo: {
+            '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+            name: 'MyOwn'
+        }
+
+    },
+
+    eureka: {
+        host: EUREKA_SERVER,
+        port: 8761,
+        servicePath: '/eureka/apps/'
+    }
+});
+
 
 // MongoDB connection
 mongoose.connect("mongodb://localhost:27017/eadp", {
@@ -23,6 +53,17 @@ mongoose.connect("mongodb://localhost:27017/eadp", {
 // Routes
 app.use("/api", OrderRoutes);
 
+// Start server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+});
+
+
+// Start Eureka Client
+eurekaClient.start((error) => {
+    if (error) {
+        console.log('Eureka registration failed:', error);
+    } else {
+        console.log('Order Payment API registered with Eureka');
+    }
 });
